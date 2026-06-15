12c1608d87e95f83ff7a6f6c9abd075213dfef80
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-06-15 06:32:46 +0530

    add Day 2 python file

---
+++ Day_2.py
@@ -0,0 +1,5 @@
+# Distance converter converting km to miles
+name = input('Enter your name :      ') 
+distance_km = float(input('Enter distance in km: '))
+distance_miles = distance_km * 0.621371
+print(f'Hello {name}, {distance_km} km is equal to {distance_miles} miles.')PS C:\Users\ASUS\python\python-101> git add .
