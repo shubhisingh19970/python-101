22ec2b8129c36a3ace86137d6a092a5c8aa66cf3
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-06-16 15:32:46 +0530

    Add Day 3

---
+++ Day_3.py
@@ -0,0 +1,8 @@
+# Slicing 
+msg = "Welcome to python 101 : Strings" 
+print (msg)
+# "1 Welcome Ring to Tyler"
+# tittle format 
+msg1 = msg [18] + ' ' + msg [:8] + msg[26:30] + ' ' + msg [8:10] + ' ' + msg[13] + msg[12] + msg[2] + msg[1] + msg[-5]
+print (msg1 . title ())
+print (msg1[::-1] . title ()) 
