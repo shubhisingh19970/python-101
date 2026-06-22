23af4c2088e0be84519cc196794e1e44cc26fc57
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-06-22 09:52:49 +0530

    Add Split and Join Exercise

--- Day_6.py/Split and Join Exercise.py
+++ Day_6.py/Split and Join Exercise.py
@@ -1,1 +1,10 @@
-print('hello')
+csv = 'Eeic,John,Michael,Terry,Graham:Terry G : Brain '
+friends_list = ['Exercise : fill me with names']
+print(friends_list)
+# From the list above fill a list (friends_list) properly
+# With the names of all the friends . One per "slot"
+# You may need to run some commands several times
+# Use print () statements to work your way through the exercise 
+friends_list = (','.join (','.join(csv.split (' : ')).split(':'))).split(' , ')
+print (friends_list)
+
