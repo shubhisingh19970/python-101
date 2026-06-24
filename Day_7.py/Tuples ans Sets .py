082edb5b327cc22109ad2ae59a5b80af1dec01d9
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-06-24 07:26:53 +0530

    Add Tuples and Set .py

---
+++ Day_7.py/Tuples ans Sets .py
@@ -0,0 +1,11 @@
+# Tuples - Faster list that can't be change 
+
+friend = ['John','Michael','Terry','Graham']
+print (friend)
+friends_tuple = ('John','Michael','Terry','Graham')
+print (friends_tuple)
+
+# Sets - Unordered collection of unique items
+friends = ['John','Michael','Terry','Graham','John']
+friends_sets = set(friends)
+friends_sets = {'John','Michael','Terry','Graham','John'}
