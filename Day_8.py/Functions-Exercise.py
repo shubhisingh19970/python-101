773bf8d5f3226135dd1a2fdaac58b1f35c8ff0d4
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-07-05 23:39:51 +0530

    Add Functions-Exercise

--- Day_8.py/Functions-Exercise.py
+++ Day_8.py/Functions-Exercise.py
@@ -1,32 +1,22 @@
-#def greeting (name,age = 28):
-    # Greets user with 'name; from 'input box' and 'age' , if available , defaault age is used
-  name = input("Enter your name: ")
-  age = input ("Enter your age:  ")
-#print ('Hello' + { name } + ', you are' + str(age) + '!')
-#print(f'Hello {name}, you are {age}!')
+def greeting(name, age=28, color = 'red'):
+    #Greets user with 'name' from 'input box' and 'age' next year, if available, default age is used
+    # also includes favorite color
+    print('Hello '  +  name.capitalize() + ', you will be ' + str(age+1) +' next birthday!')
+    print(f'Hello {name.capitalize()}, you will be {age+1} next birthday!')
+    print(f'We hear you like the color {color.lower()}!')
 
-#greeting (name,32)
-    #1 Add new print statement on a new line which says 'We hear you like the colours xxx ! xxx ia a string with colour
-#print (f'we hear you like the colour { colour } ! ')
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+color = input('Enter favorite color: ')
+greeting(name, int(age), color)
 
-    #2 Extend the function with another input parameter 'colour' , that depends to 'red'
-#def greeting ( name,age = 28 ,colour = 'red') :
+# 1. Add new print statement - on a new line
+#    which says 'We hear you like the color xxx! xxx is a string with color 
+# 2. extend the function with another  input parameter 'color', that defaults to 'red'
+# 3. Capture the color via an input box as variable:color 
+# 4. Change the 'You are xx!' text to say 'you will be xx+1 years old next birthday 
+#  adding 1 to the age
+# 5. Capitalize first letter of the 'name', and rest are small caps 
+# 6. Favorite color should be in lowercase 
+    
 
-#3 Capture the colour via an input box as variable : colour 
- #colour = input ('Enter your favourite colour: ')
-
- def greeting(name, age=28, colour='red'):
-    # Greets user with 'name' and 'age' (or 'age's default), and now 'colour' too
-    print('Hello ' + name + ', you are ' + str(age) + '!')
-    print(f'Hello {name}, you are {age}!')
-
-    # Task 1: new print statement using the colour parameter
-    print(f'We hear you like the colour {colour}!')
-
-
-# --- Main program (this runs OUTSIDE the function definition) ---
-name = input('Enter your name:  ')
-age = input('Enter your age:   ')
-colour = input('Enter your favourite colour: ')
-
-greeting(name, int(age), colour)
