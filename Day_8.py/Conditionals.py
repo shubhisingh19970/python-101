bbe5a6bca17bf97524143bbcdca0d14a32d1fd2c
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-07-07 22:43:22 +0530

    Add Conditionals

---
+++ Day_8.py/Conditionals.py
@@ -0,0 +1,21 @@
+#if,else,elif
+#if
+x = 10
+if x > 5 :
+    print("x is greater than 5")
+
+# elif
+x = 10 
+if x > 15 :
+    print ("x is greater than 15")
+elif x > 5 :
+    print("x is greater than 5 but less than 15")
+
+#else
+x = 3
+if x > 5 :
+    print("x is greater than 5")
+elif x == 5 :
+    print ("x is equal to 5")
+else :
+    print("x is less than 5")
