698822519059b09382b779e8c4701862d450c22d
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-07-11 22:59:29 +0530

    Add file

--- Day_10.py/While Loop .py
+++ Day_10.py/While Loop .py
@@ -4,8 +4,13 @@ print ("1. * Loops are great *")
 print ("2.* * Loops are great * *")
 print ("3.* * * Loops are great * * *")
 
+#Three Loop Questions :
+#1. What do I want to repeat ?
+#2. How many times do I want to repeat it ?
+#3. What is the condition for repeating it ?
+
 i = 1
 while i < 4:
-    print ("* Loops are great *")
+   
     print (f" {i} . "+ "*" * i + " Loops are great " + "*" * i)
     i = i + 1
