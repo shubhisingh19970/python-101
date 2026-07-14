598a6386cb9cb4d9527a60860fc8815624d75972
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-07-14 23:57:00 +0530

    Add For loop and nesting

--- Day_12.py/For loop and nesting .py
+++ Day_12.py/For loop and nesting .py
@@ -1,4 +1,24 @@
-for letter in ' Norwegian Blue':
-    print (letter)
-    print (" For loop done ")
+for letter in 'Norwegian Blue':
+    print ('letter ')
+    print ("For Loop done !")
     
+   
+
+for furgle in range (8) :
+    print (furgle)
+    print ("For Loop done !")
+
+    for name in ['John', 'Paul', 'George', 'Ringo']:
+        print (name)
+        print ("For Loop done !")
+        friends = ['John', 'Paul', 'George', 'Ringo']
+        for friend in friends :
+            print (friend)
+            print ("For Loop done !")
+
+        friends = ['John', 'Paul', 'George', 'Ringo']
+        for index in range (len(friends)) :
+            print (friends[index])
+            print ("For Loop done !")
+
+            
