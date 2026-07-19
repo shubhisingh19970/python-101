b3e794c659684650aea88e10460789ba173d3d17
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-07-19 21:51:24 +0530

    Add Enumerate

---
+++ Day_14.py/Enumerate.py
@@ -0,0 +1,6 @@
+friends = ['John Cleese', 'Eric Idle', 'Michael Palin', 'Graham Chapman', 'Terry Gilliam', 'Terry Jones']
+
+for friend in friends :
+    print (friend)
+# But we want to print with numbers that's where enumerators steps in ('i')
+    
