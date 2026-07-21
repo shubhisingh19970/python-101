910c9eee1e133de851068524aea613686f9878fd
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-07-21 23:45:19 +0530

    Add Enumerate

--- Day_14.py/Enumerate.py
+++ Day_14.py/Enumerate.py
@@ -3,4 +3,25 @@ friends = ['John Cleese', 'Eric Idle', 'Michael Palin', 'Graham Chapman', 'Terry
 for friend in friends :
     print (friend)
 # But we want to print with numbers that's where enumerators steps in ('i')
+
+print('python101 - Enumerate')
+friends = ['Brian', 'Judith', 'Reg', 'Loretta', 'Colin']
+
+i = 0
+for friend in friends:
+    print(i, friend)
+    i = i +1 # += 1
+
+print('python101 - Enumerate')
+friends = ['Brian', 'Judith', 'Reg', 'Loretta', 'Colin']
+efriends = [(51,'Brian'), (52,'Judith'), (53,'Reg'), (54,'Loretta'), (55,'Colin')]
+
+#i = 51
+#for friend in friends:
+#    print(i, friend)
+#    i = i +1 # += 1
+for num, friend in enumerate(friends,51):
+    print(num, friend)
     
+print(type(enumerate(friends)))
+print(list(enumerate(friends)))        
