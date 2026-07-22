beb48d95ba7330909d78c90dfeeace1eb8432e2f
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-07-22 23:01:17 +0530

    Add Short and Shorted

---
+++ Day_15.py/Short and Shorted.py
@@ -0,0 +1,9 @@
+my_list = [1,5,3,7,2]
+my_dict = {'car':4,'dog':2,'add':3,'bee':1}
+my_tuple = ('d','c','e','a','b')
+my_string = 'python'
+print(my_list,'original')
+print(sorted(my_list))
+print(my_list,'new')
+my_list1 =sorted(my_list)
+print(my_list1)
