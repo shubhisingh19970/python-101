4ec0f3093e172f471833144cbf6614d3200d7ac9
shubhisingh19970 <shubhisingh19970@gmail.com>
2026-07-31 23:36:03 +0530

    Add Dicitionaries

--- Day_18.py/Dictionaries.py
+++ Day_18.py/Dictionaries.py
@@ -4,9 +4,39 @@ movie = {
     'cast' : ['John','Eric','Michael','George','Terry']
 }
 print("{Dictionaries}")
+
 movie = {
     'title' : 'Life of Brian',
     'year' : 1979,
     'cast' : ['John','Eric','Michael','George','Terry']
 }
-print("{Dictionaries}")
+movie['title'] = 'The Holy Grail'
+movie['budget'] = 250000
+print(movie)
+
+movie = {
+    'title' : 'Life of Brian',
+    'year' : 1979,
+    'cast' : ['John','Eric','Michael','George','Terry']
+}
+movie.update({'title' : 'The Holy Grail','year':1975,'cast':['John','Eric','Michael','George','Terry']})
+movie['budget'] = 250000
+del movie['year']
+print(movie)
+
+
+movie = {
+    'title' : 'Life of Brian',
+    'year' : 1979,
+    'cast' : ['John','Eric','Michael','George','Terry']
+}
+
+print(len(movie))
+
+print(movie.keys())
+
+for key in movie:
+    print(key)
+
+for key, value in movie.items():
+    print(key, value)
